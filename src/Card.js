bc905672986c3dfb10f4ca905526b9b76a5452bb
Psalishol samuel <psalishol80@gmail.com>
2022-07-02 11:56:36 +0200

    Added props to card function

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 import 'tachyons'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="bg-light-green dib br4 ma3 pa3 grow">
-      <img src="https://robohash.org/test?200x200"  alt="component"/>
+    <div className="bg-light-green dib br4 ma2 pa3 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${props.name}?200x200`}  alt="component"/>
 
       <div>
-        <h2>Psalishol NFT</h2>
-        <p>psalishol@gmail.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.price}</p>
       </div>
     </div>
   );
