0ec28889df1c23846b663309d53a4e3cec6336d2
Psalishol samuel <psalishol80@gmail.com>
2022-07-02 11:39:37 +0200

    Robot model

---
+++ src/robot_model.js
@@ -0,0 +1,39 @@
+/*
+Gere
+Gere
+Twobit
+Wire
+Ujat
+Oyecoid
+Gage
+Robert
+Abipx
+Ohh
+Exakroid
+Gadget
+Rubber
+Shrimp
+
+*/
+const robotName = [
+    {
+        "id": '1',
+        'name': 'Gere',
+        'cost' : '78372'
+    },
+    {
+        "id": '2',
+        'name': 'Robert',
+        'cost' : '91045'
+    },
+    {
+        "id": '3',
+        'name': 'Exakroid',
+        'cost' : '38471'
+    },
+    {
+        "id": '4',
+        'name': 'Abipx',
+        'cost' : '090353'
+    }
+]
