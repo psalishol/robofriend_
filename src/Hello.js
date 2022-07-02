cbd3277790559264ccbd6dc32dec6b406ac29af5
Psalishol samuel <psalishol80@gmail.com>
2022-07-02 10:38:27 +0200

    Hello component

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+
+
+import { Component } from "react";
+
+class Hello extends Component{
+    render(){
+        return (
+           <div className="Hello App">
+              <h1 className="hello">Hello world</h1>
+              <p className="welcome to learning">Welcome to react learning</p>
+           </div>
+        );
+    }
+}
+
+
+export default Hello;
